9c53f07675f39c4c79d53dfc215028df4db4af6f
Mahesh Kulkarni <kulkarni.mak172@gmail.com>
2017-03-06 14:56:58 +0530

    Added a POC component for testing some sample code

--- components/landing-page/landing-page.controller.js
+++ components/landing-page/landing-page.controller.js
@@ -4,7 +4,10 @@
 
     var app = angular.module("SocialViewer");
     var LandingPageController = function($scope, $log) {
+    	var vm = this
 
     };
+
     app.controller("LandingPageController", LandingPageController);
+
 }());
